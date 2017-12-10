59baa0930db37a219b2e1ded9511297fb572da20
Simon Larsson <simon.larsson94@hotmail.com>
2017-12-10 18:32:51 +0100

    Edited server.js/javascript.js

---
+++ public/js/javascript.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+var url = "";
+var urlLocal = ""
+var urlAPI = "https://api.guildwars2.com/v2/items/"; // url for the external API.    
+var randNumMax = 49429; // Number of different available item id:s on GW2:s open API.
+var fromValue = 0;
+var toValue = 10;
+    
+ function generateItemId() {
+     
+     return Math.floor(Math.random()* randNumMax); //Generates a random item id.
+     
+ };
+
+    
+var itemIdResult = generateItemId();
+var itemIdResultStr = itemIdResult.toString();
+    
+
+function getData() {
+     
+     //$.getJSON("https://api.guildwars2.com/v2/items/" + (generateItemId());
+    
+    $.getJSON(urlAPI + itemIdResultStr, function(data) {
+            var jsonSliced = data.slice(fromValue, toValue); // Get first 10 objects.
+            for (var i = 0; i < jsonSliced.length; i++) {
+                var name = jsonSliced[i].name;
+                var type = jsonSliced[i].type;
+                var rarity = jsonSliced[i].rarity;
+                var iconUrl = jsonSliced[i].icon;
+                var icon = "<img src='"+ iconUrl +"' class='img-fluid'>";
+                
+                
+
+                var tr = $("<tr/>");
+                tr.append("<td></td>");
+                tr.append("<td class='name'>" + name + "</td>");
+                tr.append("<td class='type'>" + type + "</td>");
+                tr.append("<td class='rarity'>" + rarity + "</td>");
+                tr.append("<td class='icon'>" + icon + "</td>");
+                tr.append("<td width='10%'><form method='POST' action='http://localhost:3000/testdb'><input type='hidden' name='name' value='" + name + "'/><input type='hidden' name='type' value='" + type + "'/><input type='hidden' name='rarity' value='" + rarity + "'/><input type='hidden' name='icon' value='" + icon + "'/><input class='add' type='submit' value='+'/></form></td>");
+                $(".table-left").append(tr);
+            };
+        });
+    };
+    
+    
+    generateItemId();
+    getData();
+    
+    
+    
+    
+    
+    
+});
--- server.js
+++ server.js
@@ -1,21 +1,70 @@
+//Imported modules.
 var express = require('express');
 var app = express();
 var pouchDB = require('pouchdb');
 var bodyParser = require('body-parser');
-var error = Error('error');
+var path = require('path');
+
+
+//Global variables.
+var dbName = "testdb";
+var database = new pouchDB("http://127.0.0.1:5984/" + dbName);
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
+//GET-method: Get items from my database.
+app.get("/" + dbName, function (req, res) {
+    database.allDocs({
+        include_docs: true
+    }).then(function (result) {
+        res.send(result.rows.map(function (item) {
+            return item.doc;
+        }));
+    }, function (error) {
+        res.status(400).send(error);
+    });
+});
 
 
+//POST-method: Post an item to my database.
+app.post("/" + dbName, function (req, res) {
+    database.post(req.body).then(function (result) {
+        res.redirect("back"); // Redirects user to the previous page.
+    }, function (error) {
+        res.status(400).send(error);
+    });
+});
 
-app.listen(3000, function(error){
-   if (!error) {
-       console.log("Server is running on port 3000.");
-   } else {
-       console.log("Error. Something went wrong.")
-   } 
+
+// DELETE specific ID from database.
+app.delete("/" + dbName + "/:id", function (req, res) {
+    database.get(req.params.id).then(function (result) {
+        return database.remove(result);
+    }).then(function (result) {
+        res.send(result);
+    });
+
+});
+
+//GET homepage (root) route.
+app.get("/", function (req, res, error) {
+    if (!error) {
+        res.send("index.html");
+    } else {
+        console.log("404.. oh no..");
+    }
+});
+
+app.listen(3000, function (error) {
+    if (!error) {
+        console.log("Server is running on port 3000.");
+    } else {
+        console.log("Error. Something went wrong.")
+    }
 });
